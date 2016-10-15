3d5a6e4a1d5a5eabad2fb4d951b0704668e40e09
Wes Jefferson Ong <wesjong@gmail.com>
2016-10-15 04:39:40 -0400

    Initial finger tracking

--- src/signal/Main.java
+++ src/signal/Main.java
@@ -21,8 +21,29 @@ class LeapListener extends Listener {
 	public void onExit(Controller controller) {
 		System.out.println("Exited");
 	}
-	public void onFrame(Controller controller) {
-		
+
+	long lastFrameID = 0;
+
+	public void onFrame(Controller controller) {	
+		Frame frame = controller.frame();
+		Hand hand = frame.hands().rightmost();   
+		FingerList finger = hand.fingers();
+		if (!finger.isEmpty()) {
+			for (int i = 0; i < 5; i++)
+				if (finger.get(i).isValid())
+					System.out.println((i+1)+ ": " + (((Pointable) finger.get(i)).tipPosition().minus(hand.palmPosition())));
+		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+	public void checkAlpha() {
+
 	}
 }
 
@@ -33,14 +54,15 @@ public class Main {
 		LeapListener listener = new LeapListener();
 		Controller controller = new Controller();
 		controller.addListener(listener);
+
 		System.out.println("Press Enter to quit");
-		
+
 		try {
 			System.in.read();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		controller.removeListener(listener);
 	}
 
