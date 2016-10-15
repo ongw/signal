ee7587837c5451174590d2201b4dfe666057e689
Wes Jefferson Ong <wesjong@gmail.com>
2016-10-15 08:24:24 -0400

    Switching to javascript

--- src/signal/Main.java
+++ src/signal/Main.java
@@ -29,9 +29,7 @@ class LeapListener extends Listener {
 		Hand hand = frame.hands().rightmost();   
 		FingerList finger = hand.fingers();
 		if (!finger.isEmpty()) {
-			for (int i = 0; i < 5; i++)
-				if (finger.get(i).isValid())
-					System.out.println((i+1)+ ": " + (((Pointable) finger.get(i)).tipPosition().minus(hand.palmPosition())));
+			checkAlpha(finger, hand);
 		}
 		try {
 			Thread.sleep(1000);
@@ -42,7 +40,36 @@ class LeapListener extends Listener {
 	}
 
 
-	public void checkAlpha() {
+	public void checkAlpha(FingerList finger, Hand hand) {
+		float sum = 0;
+		float MIN_PREC = 20;
+		float Y_MIN_PREC = 50;
+		float MAX_PREC = 20;
+		
+		// 0 = x, 1 = y, 2 = z
+		float[][] fingerData = new float[5][3]; 
+
+		//initialize values
+		for (int i = 1; i < 5; i++)
+			if (finger.get(i).isValid()) {
+				fingerData[i][0] =  Math.abs(finger.get(i).tipPosition().minus(hand.palmPosition()).getX());
+				fingerData[i][1] =  Math.abs(finger.get(i).tipPosition().minus(hand.palmPosition()).getY());
+				fingerData[i][2] =  Math.abs(finger.get(i).tipPosition().minus(hand.palmPosition()).getZ());
+			}
+
+		//A
+		if ((fingerData[1][0]+fingerData[2][0]+fingerData[3][0]+fingerData[4][0])/4 < MIN_PREC && 
+				(fingerData[1][1]+fingerData[2][1]+fingerData[3][1]+fingerData[4][1])/4 < Y_MIN_PREC && 
+				(fingerData[1][2]+fingerData[2][2]+fingerData[3][2]+fingerData[4][2])/4 < MIN_PREC)
+			System.out.println("A");
+		
+		//B
+		else if ((fingerData[0][0]+fingerData[0][2])/2 < MIN_PREC && 
+				(fingerData[1][0]+fingerData[1][2] + fingerData[2][0]+fingerData[2][2] + fingerData[3][0]+
+						fingerData[3][2] + fingerData[4][0]+fingerData[4][2])/10 < MIN_PREC &&
+				(fingerData[1][1]+fingerData[2][1]+fingerData[3][1]+fingerData[4][1])/4 > MAX_PREC)
+			System.out.println("B");
+		else System.out.println("Z");
 
 	}
 }
